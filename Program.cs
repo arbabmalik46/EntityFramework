d308c54c439ce3138075869c70a11a05c0940b18
arbabmalik46 <arbabmalik46@gmail.com>
2024-05-21 12:53:26 +0500

    Added different projects

--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
-
+            var context = new BusinessEntities();
+            var prsn = new PRSN()
+            {
+                FirstName = "Sana",
+                LastName = "Sheikh",
+                Address = "Ali Town",
+                City = "Lahore"//,
+                //PersonID = 501
+            };
+            context.PRSNs.Add(prsn);
+            context.SaveChanges();
         }
     }
 }
