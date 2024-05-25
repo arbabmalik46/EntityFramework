a85e7d0f092e6590b65456529c295e377280316d
arbabmalik46 <arbabmalik46@gmail.com>
2024-05-25 20:11:48 +0500

    Practiced the DB First

---
+++ ExerciseDBFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDBFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context =new PracticeDBEntities();
+            Genre genre = new Genre()
+            {
+                GenreID = 1,
+                Name = "Funny",
+            };
+            context.SaveChanges();
+        }
+    }
+}
