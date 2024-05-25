89eb43212771387f0a743185154879fb71bc9fe5
arbabmalik46 <arbabmalik46@gmail.com>
2024-05-25 12:19:54 +0500

    Added Multiple Projects

---
+++ CodeFirstApproachIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproachIntro
+{
+    public class BlogDBContext : DbContext
+    {
+        public DbSet<Posts> Posts { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ DBFirst/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Executing Method
+            //var plutoContext = new PlutoDbContext();
+            //var courses = plutoContext.GetCourses(); 
+            //SslStream sslStream = null;
+            //sslStream.ReadTimeout = 0;
+            //foreach (var item in courses)
+            //{
+            //    Console.WriteLine(item.Title + " "+ item.CourseID + " "+item.LevelString+" "+item.Price);
+            //}
+            //Console.Read();
+            #endregion
+
+            #region Execute the context
+            var course = new Courses();
+            course.Level = CourseLevel.Begineer;
+            var CourseContext = new PlutoDbContext();
+            CourseContext.Courses.Add(course);
+            try
+            {
+                int ad = CourseContext.SaveChanges();
+
+                if (ad > 0)
+                {
+                    Console.WriteLine("Work Done");
+                    Console.Read();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+            
+            #endregion
+        }
+    }
+}
